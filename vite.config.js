b3543f372284c4d3ebdd19a9ca1754a1120c4404
florentduveau <florent.duveau@gmail.com>
2021-10-11 10:12:27 +0200

    add build config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+const path = require("path");
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "lib/index.jsx"),
+      name: "Underwood",
+      fileName: (format) => `underwood.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});
