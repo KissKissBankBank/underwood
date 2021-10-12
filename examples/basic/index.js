89b9a653cda7ed1d0d8776f20ed1d6e17662b865
florentduveau <florent.duveau@gmail.com>
2021-10-12 13:20:07 +0200

    externalize more dependencies

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -12,6 +12,9 @@ const Index = () => {
             <Controls.Italic />
             <Controls.Bold />
           </Controls.Group>
+          <Controls.Group>
+            <Controls.Video embedlyApiKey="8b7d8dd6504d41af9a77662672aabc2a" />
+          </Controls.Group>
         </Controls>
         <Playground />
       </Editor>
