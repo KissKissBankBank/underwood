23f48bb19f7154d8b8696ca1ca835321a6c8bc76
florentduveau <florent.duveau@gmail.com>
2022-02-24 12:13:02 +0100

    bump packages

--- lib/controls/image.jsx
+++ lib/controls/image.jsx
@@ -1,27 +1,27 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import styled from 'styled-components'
-import PropTypes from 'prop-types'
-import { ErrorMessage, Formik, useField, useFormikContext } from 'formik'
-import { AtomicBlockUtils, EditorState } from 'draft-js'
-import isEmpty from 'lodash/fp/isEmpty'
-import * as Yup from 'yup'
-import { Label, InputWithButton, SubmitLoader } from '../components/form'
-import ButtonEditor from '../components/button'
 import {
-  Title,
   Field,
-  Text,
   ModalNext as Modal,
-} from '@kisskissbankbank/kitten'
-import { EditorContext, updateEditor } from '../context'
+  Text,
+  Title,
+} from "@kisskissbankbank/kitten";
+import classNames from "classnames";
+import { AtomicBlockUtils, EditorState } from "draft-js";
+import { ErrorMessage, Formik, useField, useFormikContext } from "formik";
+import isEmpty from "lodash/fp/isEmpty";
+import PropTypes from "prop-types";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+import * as Yup from "yup";
+import ButtonEditor from "../components/button";
+import { InputWithButton, Label, SubmitLoader } from "../components/form";
+import { EditorContext, updateEditor } from "../context";
 import {
   getImageUrl,
   hasEntityFocus,
   moveSelectionTo,
   removeDataFromEntity,
-} from '../utils'
-import classNames from 'classnames'
-import LinkInline from './link-inline'
+} from "../utils";
+import LinkInline from "./link-inline";
 
 const StyledImage = styled.div`
   width: 100%;
@@ -30,34 +30,34 @@ const StyledImage = styled.div`
     height: auto;
     max-width: 100%;
   }
-`
+`;
 
 const LinkManager = ({ url, entityKey }) => {
-  const [{ editorState }, dispatch] = useContext(EditorContext)
+  const [{ editorState }, dispatch] = useContext(EditorContext);
   return (
     <LinkInline
       url={url}
       onDelete={() => {
         dispatch(
-          updateEditor(removeDataFromEntity(editorState, entityKey, ['url'])),
-        )
+          updateEditor(removeDataFromEntity(editorState, entityKey, ["url"]))
+        );
       }}
     />
-  )
-}
+  );
+};
 
 const ImageEditor = ({ contentState, entityKey, blockKey }) => {
-  const [{ editorState }, dispatch] = useContext(EditorContext)
-  const hasFocus = hasEntityFocus(contentState, editorState, entityKey)
-  const { src, url } = contentState.getEntity(entityKey).getData()
+  const [{ editorState }, dispatch] = useContext(EditorContext);
+  const hasFocus = hasEntityFocus(contentState, editorState, entityKey);
+  const { src, url } = contentState.getEntity(entityKey).getData();
   const onClick = () => {
-    dispatch(updateEditor(moveSelectionTo(editorState, blockKey)))
-  }
+    dispatch(updateEditor(moveSelectionTo(editorState, blockKey)));
+  };
   return (
     <StyledImage className="kiss-Draft__image" onClick={onClick}>
       <div
-        className={classNames('kiss-Draft__image-focus', {
-          'kiss-Draft__image-focus__focused': hasFocus,
+        className={classNames("kiss-Draft__image-focus", {
+          "kiss-Draft__image-focus__focused": hasFocus,
         })}
       >
         <div className="k-u-align-center">
@@ -66,11 +66,11 @@ const ImageEditor = ({ contentState, entityKey, blockKey }) => {
         {hasFocus && !!url && <LinkManager url={url} entityKey={entityKey} />}
       </div>
     </StyledImage>
-  )
-}
+  );
+};
 
 const ImageDisplayer = ({ contentState, entityKey }) => {
-  const { src, url } = contentState.getEntity(entityKey).getData()
+  const { src, url } = contentState.getEntity(entityKey).getData();
   return (
     <StyledImage className="kiss-Draft__image-read">
       {url ? (
@@ -85,40 +85,40 @@ const ImageDisplayer = ({ contentState, entityKey }) => {
         </div>
       )}
     </StyledImage>
-  )
-}
+  );
+};
 
 const HiddenInput = styled.input`
   display: none;
-`
+`;
 
 const imageStrategy = (contentBlock, callback, contentState) => {
   contentBlock.findEntityRanges((character) => {
-    const entityKey = character.getEntity()
+    const entityKey = character.getEntity();
     return (
       entityKey !== null &&
-      contentState.getEntity(entityKey).getType() === 'IMAGE'
-    )
-  }, callback)
-}
+      contentState.getEntity(entityKey).getType() === "IMAGE"
+    );
+  }, callback);
+};
 
 export const decorator = {
   strategy: imageStrategy,
   component: ImageEditor,
-}
+};
 
 export const readDecorator = {
   strategy: imageStrategy,
   component: ImageDisplayer,
-}
+};
 
 const Form = ({ imageUrl, setImageUrl }) => {
-  const [{ translations }] = useContext(EditorContext)
-  const { handleSubmit, values, isSubmitting } = useFormikContext()
-  const [, , fileSizeHelpers] = useField('fileSize')
-  const [, , urlHelpers] = useField('url')
-  const [, , fileHelpers] = useField('file')
-  const fileInputRef = useRef(null)
+  const [{ translations }] = useContext(EditorContext);
+  const { handleSubmit, values, isSubmitting } = useFormikContext();
+  const [, , fileSizeHelpers] = useField("fileSize");
+  const [, , urlHelpers] = useField("url");
+  const [, , fileHelpers] = useField("file");
+  const fileInputRef = useRef(null);
   return (
     <>
       <HiddenInput
@@ -127,18 +127,18 @@ const Form = ({ imageUrl, setImageUrl }) => {
         type="file"
         id="imageFile"
         onChange={(event) => {
-          const file = event.target.files[0]
-          if (!file.type.match('image.*')) {
-            return
+          const file = event.target.files[0];
+          if (!file.type.match("image.*")) {
+            return;
           }
-          fileHelpers.setValue(file)
-          fileSizeHelpers.setValue(file.size)
-          urlHelpers.setValue('')
-          const reader = new FileReader()
-          reader.addEventListener('load', (file) => {
-            setImageUrl(file.target.result)
-          })
-          reader.readAsDataURL(file)
+          fileHelpers.setValue(file);
+          fileSizeHelpers.setValue(file.size);
+          urlHelpers.setValue("");
+          const reader = new FileReader();
+          reader.addEventListener("load", (file) => {
+            setImageUrl(file.target.result);
+          });
+          reader.readAsDataURL(file);
         }}
         accept="image/gif, image/jpeg, image/jpg, image/png"
       />
@@ -148,10 +148,10 @@ const Form = ({ imageUrl, setImageUrl }) => {
       <InputWithButton
         name="url"
         placeholder="https://"
-        value={translations.image_upload.preview}
+        buttonValue={translations.image_upload.preview}
         onClick={() => {
-          setImageUrl(values.url)
-          fileSizeHelpers.setValue(0)
+          setImageUrl(values.url);
+          fileSizeHelpers.setValue(0);
         }}
       />
       <div className="k-u-margin-vertical-single">
@@ -169,8 +169,8 @@ const Form = ({ imageUrl, setImageUrl }) => {
             href="#"
             className="k-u-link k-u-link-primary1"
             onClick={(e) => {
-              e.preventDefault()
-              fileInputRef.current.click()
+              e.preventDefault();
+              fileInputRef.current.click();
             }}
           >
             <Text tag="span" decoration="underline">
@@ -201,19 +201,19 @@ const Form = ({ imageUrl, setImageUrl }) => {
         </Modal.Actions>
       </div>
     </>
-  )
-}
+  );
+};
 
 const ImageControls = ({ disabled, onUpload }) => {
-  const [modalOpened, openModal] = useState(false)
+  const [modalOpened, openModal] = useState(false);
   const [{ editorState, translations, disabled: contextDisabled }, dispatch] =
-    useContext(EditorContext)
-  const [imageUrl, setImageUrl] = useState('')
+    useContext(EditorContext);
+  const [imageUrl, setImageUrl] = useState("");
   useEffect(() => {
     if (!modalOpened) {
-      setImageUrl('')
+      setImageUrl("");
     }
-  }, [modalOpened])
+  }, [modalOpened]);
   return (
     <>
       <ButtonEditor
@@ -221,9 +221,9 @@ const ImageControls = ({ disabled, onUpload }) => {
         disabled={contextDisabled || disabled}
         onToggle={() => {
           if (modalOpened) {
-            openModal(false)
+            openModal(false);
           } else {
-            openModal(true)
+            openModal(true);
           }
         }}
       />
@@ -238,72 +238,72 @@ const ImageControls = ({ disabled, onUpload }) => {
           return (
             <Modal.Block>
               <Formik
-                initialValues={{ url: '', fileSize: 0 }}
+                initialValues={{ url: "", fileSize: 0 }}
                 validationSchema={Yup.object().shape({
                   url: Yup.string()
                     .url(translations.image.invalid_url)
                     .matches(
                       /(?:jpg|jpeg|gif|png)$/,
-                      translations.image.invalid_extension,
+                      translations.image.invalid_extension
                     ),
                   fileSize: Yup.number().max(
                     5000000, // 5 Mo
-                    translations.image.max_size,
+                    translations.image.max_size
                   ),
                 })}
                 onSubmit={({ url, fileSize, file }) => {
                   return new Promise((resolve, reject) => {
                     if (isEmpty(url) && fileSize === 0) {
-                      reject('WRONG')
+                      reject("WRONG");
                     }
                     if (!isEmpty(url) && fileSize === 0) {
-                      resolve(url)
+                      resolve(url);
                     }
-                    onUpload(file).then((url) => resolve(url))
+                    onUpload(file).then((url) => resolve(url));
                   }).then((url) => {
-                    const contentState = editorState.getCurrentContent()
+                    const contentState = editorState.getCurrentContent();
                     const contentStateWithEntity = contentState.createEntity(
-                      'IMAGE',
-                      'IMMUTABLE',
+                      "IMAGE",
+                      "IMMUTABLE",
                       {
                         src: url,
-                      },
-                    )
+                      }
+                    );
                     const entityKey =
-                      contentStateWithEntity.getLastCreatedEntityKey()
+                      contentStateWithEntity.getLastCreatedEntityKey();
                     const newEditorState = AtomicBlockUtils.insertAtomicBlock(
                       editorState,
                       entityKey,
-                      ' ',
-                    )
+                      " "
+                    );
                     const newState = EditorState.forceSelection(
                       newEditorState,
-                      newEditorState.getCurrentContent().getSelectionAfter(),
-                    )
-                    dispatch(updateEditor(newState))
-                    close()
-                    openModal(false)
-                    setTimeout(() => openModal(false), 500)
-                  })
+                      newEditorState.getCurrentContent().getSelectionAfter()
+                    );
+                    dispatch(updateEditor(newState));
+                    close();
+                    openModal(false);
+                    setTimeout(() => openModal(false), 500);
+                  });
                 }}
               >
                 {() => <Form imageUrl={imageUrl} setImageUrl={setImageUrl} />}
               </Formik>
             </Modal.Block>
-          )
+          );
         }}
       </Modal>
     </>
-  )
-}
+  );
+};
 
 ImageControls.propTypes = {
   disabled: PropTypes.bool,
   onUpload: PropTypes.func,
-}
+};
 
 ImageControls.defaultProps = {
   disabled: false,
   onUpload: () => null,
-}
-export default ImageControls
+};
+export default ImageControls;
