2e72b062ffe46a3abdecba0d469889c1fc2bc078
Joachim <joachim.robert@protonmail.com>
2022-05-03 12:11:05 +0200

    Better link menu

--- lib/controls/link-inline.jsx
+++ lib/controls/link-inline.jsx
@@ -3,7 +3,7 @@ import { ArrowContainer, COLORS, pxToRem, Text } from '@kisskissbankbank/kitten'
 import styled from 'styled-components'
 import { EditorContext } from '../context'
 
-const StyledArrowContainer = styled(ArrowContainer)`
+const Wrapper = styled(ArrowContainer)`
   display: flex;
   position: absolute;
   min-width: max-content;
@@ -12,45 +12,40 @@ const StyledArrowContainer = styled(ArrowContainer)`
   margin-top: ${pxToRem(0)};
   transition: opacity 0.1s ease-out, margin-top 0.1s ease-out;
   z-index: 99999;
-`
 
-const DeleteLink = styled(Text)`
-  flex: 1;
-  width: 100%;
-  text-align: center;
-  display: block;
-  padding: 0;
+  .u-link-delete {
+    flex: 1 0 auto;
+    text-align: center;
+    display: block;
+  }
+  .u-link-separator {
+    flex: 0 0 auto;
+    border: none;
+    border-left: var(--border);
+    margin: ${pxToRem(5)} ${pxToRem(10)};
+  }
+  .u-link-visit {
+    flex: 2 0 auto;
+    max-width: ${pxToRem(240)};
+  }
 `
-
-const VerticalSeparator = styled.span`
-  flex: 0;
-  border-left: var(--border-width, 1px) solid ${COLORS.font2};
-  margin: ${pxToRem(5)} ${pxToRem(10)};
-`
-
-const ShareLink = styled(Text)`
-  flex: 2;
-  max-width: ${pxToRem(240)};
-  text-overflow: ellipsis;
-  display: inline-block;
-  overflow: hidden;
-  white-space: nowrap;
-`
-
 const LinkInline = ({ onDelete, url }) => {
   const [{ translations }] = useContext(EditorContext)
   return (
-    <StyledArrowContainer
+    <Wrapper
       position="top"
       shadow
       borderWidth={1}
-      borderColor={COLORS.line1}
       contentEditable={false}
+      borderColor="var(--color-grey-400)"
+      color="var(--color-grey-000)"
+      borderRadius={4}
     >
-      <DeleteLink
-        href="#"
-        tag="a"
-        size="micro"
+      <Text
+        className="u-link u-link-delete k-u-reset-button"
+        weight="regular"
+        tag="button"
+        type="button"
         color="error"
         onClick={(e) => {
           e.preventDefault()
@@ -59,14 +54,15 @@ const LinkInline = ({ onDelete, url }) => {
         }}
       >
         {translations.link.button.delete}
-      </DeleteLink>
-      <VerticalSeparator />
-      <ShareLink
+      </Text>
+      <hr className="u-link-separator" />
+      <Text
+        className="u-link u-link-visit k-u-ellipsis k-u-link"
+        weight="regular"
         href={url}
         target="_blank"
         rel="noopener"
         tag="a"
-        size="micro"
         color="font1"
         onClick={(e) => {
           e.preventDefault()
@@ -75,8 +71,8 @@ const LinkInline = ({ onDelete, url }) => {
         }}
       >
         {url}
-      </ShareLink>
-    </StyledArrowContainer>
+      </Text>
+    </Wrapper>
   )
 }
 
